3d6bf2d44724a842e0d504a6071f1264986a7f22
Dmitri Zagidulin <dzagidulin@gmail.com>
2016-08-03 16:13:54 -0400

    Bump rdflib deps

--- test/unit/authorization-test.js
+++ test/unit/authorization-test.js
@@ -1,7 +1,7 @@
 'use strict'
 
 var test = require('tape')
-var rdf = require('rdflib')
+// var rdf = require('rdflib')
 var Authorization = require('../../src/authorization')
 var acl = Authorization.acl
 
